b077eb036d57727e2cbf0b7a3ff34d0d334004f1
Byron Buyck <bbuyck1@gmail.com>
2016-11-27 18:40:31 -0500

    Blog App
    This is a blog app I created while taking an online Web Developer
    Course. Using PHP and mySQL to create a database of users, once you
    login, you can essentially store content so that the next time you log
    in, your post will still be there.

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("localhost", "cl59-secretd-87r", "WC^q4rtfH", "cl59-secretd-87r");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
